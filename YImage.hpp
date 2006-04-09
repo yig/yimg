9e23f5e7627e3c74b4fd4d94406f81d3c8cc9132
yotam <yotam>
2006-04-09 19:27:01 +0000

    tabs -> spaces email address obscured

--- YImage.hpp
+++ YImage.hpp
@@ -7,42 +7,42 @@ public:
     // file loading/saving automatically picks up changes to this struct.
     // The possibilities are: ARGB, ABGR, RGBA, BGRA.
     struct YPixel {
-    	unsigned char r ;
-    	unsigned char g ;
-    	unsigned char b ;
-    	unsigned char a ;
+        unsigned char r ;
+        unsigned char g ;
+        unsigned char b ;
+        unsigned char a ;
     };
     
-	YImage() ;
-	YImage( const YImage& ) ;
-	virtual ~YImage() ;
-	
-	YImage& operator=( const YImage& ) ;
-	
-	bool save( const char* fname ) const ;
-	bool load( const char* fname ) ;
-	
-	YPixel* data() ;
-	const YPixel* data() const ;
-	
-	YPixel& at( int i, int j ) ;
-	const YPixel& at( int i, int j ) const ;
-	
-	int width() const ;
-	int height() const ;
-	void resize( int width, int height ) ;
-	
-	// flip vertically
-	void flip() ;
-	// flip horizontally
-	void mirror() ;
-	// average rgb
-	void greyscale() ;
-	
+    YImage() ;
+    YImage( const YImage& ) ;
+    virtual ~YImage() ;
+    
+    YImage& operator=( const YImage& ) ;
+    
+    bool save( const char* fname ) const ;
+    bool load( const char* fname ) ;
+    
+    YPixel* data() ;
+    const YPixel* data() const ;
+    
+    YPixel& at( int i, int j ) ;
+    const YPixel& at( int i, int j ) const ;
+    
+    int width() const ;
+    int height() const ;
+    void resize( int width, int height ) ;
+    
+    // flip vertically
+    void flip() ;
+    // flip horizontally
+    void mirror() ;
+    // average rgb
+    void greyscale() ;
+    
 protected:
-	int m_width ;
-	int m_height ;
-	YPixel* m_data ; // raw image data
+    int m_width ;
+    int m_height ;
+    YPixel* m_data ; // raw image data
 };
 
 #endif /* __YImage_hpp__ */
