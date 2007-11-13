02d551106145dbaafeca3df8e1675ef14a7d21f9
yotam <yotam>
2007-11-13 16:47:31 +0000

    Can save a PNG with less compression, but faster (ASAP).  Added methods to check if two images are the same.

--- YImage.hpp
+++ YImage.hpp
@@ -14,35 +14,41 @@ public:
     };
     
     YImage() ;
-    YImage( const YImage& ) ;
-    virtual ~YImage() ;
-    
-    YImage& operator=( const YImage& ) ;
-    
-    bool save( const char* fname ) const ;
-    bool load( const char* fname ) ;
-    
-    YPixel* data() ;
-    const YPixel* data() const ;
-    
-    YPixel& at( int i, int j ) ;
-    const YPixel& at( int i, int j ) const ;
-    
-    int width() const ;
-    int height() const ;
-    void resize( int width, int height ) ;
-    
-    // flip vertically
-    void flip() ;
-    // flip horizontally
-    void mirror() ;
-    // average rgb
-    void greyscale() ;
-    
+	YImage( const YImage& ) ;
+	virtual ~YImage() ;
+	
+	YImage& operator=( const YImage& ) ;
+	
+	// Returns true if the images are the same size and have the same data (for r,g,b,a).
+	bool same( const YImage& rhs ) const ;
+	// Returns true if the images are the same size and have the same data for r,g,b.
+	bool same_rgb( const YImage& rhs ) const ;
+	
+	// Saves a PNG image.  If 'asap' is true, sacrifices compression ratio for speed.
+	bool save( const char* fname, bool asap = false ) const ;
+	bool load( const char* fname ) ;
+	
+	YPixel* data() ;
+	const YPixel* data() const ;
+	
+	YPixel& at( int i, int j ) ;
+	const YPixel& at( int i, int j ) const ;
+	
+	int width() const ;
+	int height() const ;
+	void resize( int width, int height ) ;
+	
+	// flip vertically
+	void flip() ;
+	// flip horizontally
+	void mirror() ;
+	// average rgb
+	void greyscale() ;
+	
 protected:
-    int m_width ;
-    int m_height ;
-    YPixel* m_data ; // raw image data
+	int m_width ;
+	int m_height ;
+	YPixel* m_data ; // raw image data
 };
 
 #endif /* __YImage_hpp__ */
